9008c6d8662aab49ecdf853147c5dcd39bbd9243
igbinosa iwinosa <igbinosaiwinosa01@gmail.com>
2021-10-04 23:27:53 +0100

    navbar edit

--- js/script.js
+++ js/script.js
@@ -2,9 +2,19 @@
 
 const nav = document.getElementsByClassName('nav')[0]
 const toggle = document.getElementsByClassName('toggle-icon')[0]
+const bars = document.getElementsByClassName('bars')[0]
+const times = document.getElementsByClassName('times')[0]
 
-toggle.onclick = function () {
+bars.onclick = function () {
   'use strict'
-
+  nav.classList.toggle('collapse');
+   times.classList.add('show')
+  bars.classList.add('hide');
+}
+times.onclick = function () {
+  'use strict'
   nav.classList.toggle('collapse')
+   times.classList.remove('show')
+  bars.classList.remove('hide')
+
 }
