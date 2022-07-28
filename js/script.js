9165d4f03016c614ab9816498f66d8bf65aa23c3
Favourene <igbinosaiwinosa01@gmail.com>
2022-07-28 18:06:57 +0100

    mAJOR EDIT

--- js/script.js
+++ js/script.js
@@ -7,14 +7,14 @@ const times = document.getElementsByClassName('times')[0]
 
 bars.onclick = function () {
   'use strict'
-  nav.classList.toggle('collapse');
-   times.classList.add('show')
-  bars.classList.add('hide');
+  nav.classList.toggle('collapse')
+  times.classList.add('show')
+  bars.classList.add('hide')
 }
 times.onclick = function () {
   'use strict'
   nav.classList.toggle('collapse')
-   times.classList.remove('show')
+  times.classList.remove('show')
   bars.classList.remove('hide')
-
 }
+AOS.init()
